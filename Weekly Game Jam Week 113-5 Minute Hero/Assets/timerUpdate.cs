8313c1e1766e3a4b53e9a6ba519ffd06e8056bba
sondrex <sondrewr@stud.ntnu.no>
2019-09-06 22:22:58 +0200

    Added countdown timer to screen overlay

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/timerUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timerUpdate : MonoBehaviour
+{
+    public Text textOverlay;
+    float remainingTime = 300;
+
+    // Update is called once per frame
+    void Update()
+    {
+        remainingTime -= Time.deltaTime;    // updates time value
+
+        // gets minutes, secodns and milliseconds
+        int minutes, seconds, milliseconds;
+        minutes = (int)remainingTime / 60;
+        seconds = (int)remainingTime % 60;
+        milliseconds = (int)((remainingTime - (int)remainingTime) * 100);
+
+        textOverlay.text = minutes + ":" + seconds + ":" + milliseconds;
+    }
+}
