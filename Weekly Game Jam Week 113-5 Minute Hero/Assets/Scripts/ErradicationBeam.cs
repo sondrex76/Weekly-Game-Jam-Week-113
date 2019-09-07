f6abb9c5ef7a19b2f2280315b0c9954e6edf9e6b
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 22:00:44 +0200

    The flying opponents now work as intended, the game has the minimum level of features, yay

--- Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/ErradicationBeam.cs
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/ErradicationBeam.cs
@@ -8,12 +8,15 @@ public class ErradicationBeam : MonoBehaviour
     public Light outerLight, innerLight, spotLight; // Lights
 
     public float distanceLight;                     // Distance of lights from parent object
-    public float timeShooting = 5;                  // Time it takes for ranged opponents to shoot
+    public float timeShooting = 7.5f;               // Time it takes for ranged opponents to shoot
     public float erradicationDamage = 80;           // The damage taken from a direct hit
+    public float waitingTimeLights = 1.0f;          // Duration of lighting up
 
     bool targetFound = false;                       // bool to see if the target have been found
+    bool lightOn = false;                           // Bool for if the lights areis on
     float timeWaited;                               // The time waited since it were allowed to shoot
     float currentY;                                 // current y of the direction towards the player
+    float waitTurnOffLights = 0;                    // time waited since shot went off
 
     // Update is called once per frame
 
@@ -31,6 +34,20 @@ public class ErradicationBeam : MonoBehaviour
         // When the osund ends, shoot the beam, lighting up everything in that general direaction
         // If the player vere in a direct line from the ranged opponent, they take a large amount of damage
 
+        // Lets the lights stay on for a certain perios of time
+        if (lightOn)
+        {
+            if (waitTurnOffLights > waitingTimeLights)
+            {
+                lightOn = outerLight.enabled = innerLight.enabled = spotLight.enabled = false;
+                waitTurnOffLights = 0;
+            }
+            else
+            {
+                waitTurnOffLights += Time.deltaTime;
+            }
+        }
+
         // Well, it places the lights in front of the player, which isn't exactly what it were supposed to do
         Vector3 playerPos = Camera.main.transform.position; // Gets player's position
         currentY = playerPos.y;                             // Sets currentY
@@ -45,6 +62,10 @@ public class ErradicationBeam : MonoBehaviour
             timeWaited += Time.deltaTime;
             if (timeWaited >= timeShooting) // If enough time have passed
             {
+                // Updates lights
+                lightOn = true;
+                outerLight.enabled = spotLight.enabled = true;
+
                 RaycastHit hit;
 
                 transform.LookAt(Camera.main.transform.position);   // Gets direction
@@ -81,6 +102,7 @@ public class ErradicationBeam : MonoBehaviour
                 {
                     if (hit.transform.tag == "MainCamera")  // If it was indeed the player, they are in range and in direct line of sight to the ranged opponent
                     {
+                        innerLight.enabled = true;
                         shootingNoise.Play();
                         targetFound = true;         // Updates targetFound
                     }
