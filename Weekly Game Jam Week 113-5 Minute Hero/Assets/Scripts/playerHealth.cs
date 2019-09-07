77294f03ed760c4381701170beafa211afb70397
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 11:26:46 +0200

    Added detection and health loss when colliding with enemies

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour
+{
+    // UI elements
+    public Slider healthFill;
+
+    // Public variables
+    public float currentHealth;             // Current health
+    public float maxHealth;                 // Max health
+    public float damageCollision;           // Damage taken when colliding with an enemy
+
+    // Should trigger when something stays within the collider
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "Enemy") // checks if other is enemy
+        {
+            ChangeHealth(-damageCollision * Time.deltaTime);
+            Debug.Log("Triggered TEST");
+        }
+    }
+
+    // Updates health
+    public void ChangeHealth(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        healthFill.value = currentHealth;
+
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+    
+    // Detah have been reached
+    void Die()
+    {
+        // implement a death sequence of some sort
+    }
+}
