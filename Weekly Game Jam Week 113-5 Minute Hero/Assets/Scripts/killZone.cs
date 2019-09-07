60c8004a1ffbca2b34f82b9c8073af8190c2f8af
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 13:48:58 +0200

    Added killzone below so both the player and enmies die if they fall

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/killZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killZone : MonoBehaviour
+{
+    // whenever something enters the collider it dies
+    private void OnTriggerStay(Collider other)
+    {
+        // divided into two because I am not sure if it is possible to make it into one(even though the function it calls has the exact same name
+        if (other.transform.tag == "Enemy")
+            other.gameObject.GetComponent<HealthValue>().ChangeHealth(-800);  // Removes 800hp, should insta kill anything
+        else if (other.transform.tag == "MainCamera")
+            other.gameObject.GetComponent<playerHealth>().ChangeHealth(-800);  // Removes 800hp, should insta kill anything
+    }
+}
