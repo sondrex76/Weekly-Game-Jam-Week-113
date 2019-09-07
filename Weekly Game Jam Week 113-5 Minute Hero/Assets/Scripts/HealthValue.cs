170b2134d6dc49ac682b88b60cb3e10999426382
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 09:05:52 +0200

    Made healthbar work and fixed the healthbar

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/HealthValue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthValue : MonoBehaviour
+{
+    // UI elements
+    public Transform healthBar;
+    public Slider healthFill;
+
+    // Public variables
+    public float currentHealth;             // Current health
+    public float maxHealth;                 // Max health
+    public float healthBarYOffset = 0.0f;   // Health bar Y offset
+
+    public void ChangeHealth(int amount) {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        healthFill.value = currentHealth;
+    }
+
+    // updates the health bar's position
+    private void PositionHealthBar() {
+        Vector3 currentPos = transform.position;
+        healthBar.position = new Vector3(currentPos.x, currentPos.y + healthBarYOffset, currentPos.z);
+
+        healthBar.LookAt(Camera.main.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PositionHealthBar();
+    }
+}
