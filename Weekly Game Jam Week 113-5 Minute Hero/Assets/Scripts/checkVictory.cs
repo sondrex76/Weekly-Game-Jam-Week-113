a6509e3fcb5b17561922400d9ca8912d453b5f0b
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 13:20:07 +0200

    Added victory condition(survive and kill everything within 5 minutes)

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/checkVictory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checkVictory : MonoBehaviour
+{
+    void Update()
+    {
+        if (transform.childCount == 0) {    // Checks if all children(enemies) are dead
+            SceneManager.LoadScene("YouWon");
+        }
+    }
+}
