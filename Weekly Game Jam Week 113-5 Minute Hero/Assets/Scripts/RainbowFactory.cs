bb8c24ffc4139bbd42ae358504a652eacee5d654
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 18:30:36 +0200

    Added background sound to the death screen

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/RainbowFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowFactory : MonoBehaviour
+{
+    public AudioSource cursedSound;
+    public AudioClip cursed1;
+    public AudioClip cursed2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.Range(1, 10) > 5)
+            cursedSound.clip = cursed1;
+        else
+            cursedSound.clip = cursed2;
+
+        cursedSound.Play(); // Starts playing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/walkingSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class walkingSound : MonoBehaviour
+{
+    public AudioSource audio;
+
+    Vector3 previousPosition = new Vector3(0, 0, 0);
+    
+    // plays sound if the character's movement is not approximately 0
+    void Update()
+    {
+        if (previousPosition != transform.position) // if teh character have moved
+        {
+            audio.mute = false;
+            Debug.Log(transform.position);    // DEBUG
+
+            previousPosition = transform.position;  // updates position
+        }
+        else
+        {
+            audio.mute = true;
+            Debug.Log("stopped");
+        }
+    }
+}
