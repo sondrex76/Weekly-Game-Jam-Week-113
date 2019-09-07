91c7e34543dd5c0fd736243ee11935704251ffd7
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 17:08:44 +0200

    Added flash when shooting with gun and slightly increased running speed

--- Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/Gun.cs
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/Gun.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public Camera cameraObject;
+    public Light innerLight;
+    public Light outerLight;
 
-    public float damage = 10f;  // Damage per shot
-    public float range = 150f;  // Range for weapon
+    public float minimumLightTime = 0.1f;   // Minimum time the flash lasts
+    public float damage = 10f;              // Damage per shot
+    public float range = 150f;              // Range for weapon
+
+    float timeRun = 0;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))   // Shoot and enable light flash
         {
+            innerLight.enabled = outerLight.enabled = true;
             Shoot();
+        }
+        else {                              // Disable light flash
+            timeRun += Time.deltaTime;
+            if (timeRun >= minimumLightTime)
+            {
+                innerLight.enabled = outerLight.enabled = false;
+                timeRun = 0;
+            }
         }
     }
 
@@ -23,11 +37,9 @@ public class Gun : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
             HealthValue enemy = hit.transform.GetComponent<HealthValue>();
             if (enemy != null)
             {
-                Debug.Log("TEST");
                 enemy.ChangeHealth(-damage);
             }
         }
