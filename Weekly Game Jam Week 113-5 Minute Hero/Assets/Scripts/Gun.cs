5fdb754563196ebddf23b52649253b1b039f7a5b
sondrex <sondrewr@stud.ntnu.no>
2019-09-07 10:17:07 +0200

    Added hitscap capabilities to the gun

---
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Camera cameraObject;
+
+    public float damage = 10f;  // Damage per shot
+    public float range = 150f;  // Range for weapon
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    // Shoots with gun
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
