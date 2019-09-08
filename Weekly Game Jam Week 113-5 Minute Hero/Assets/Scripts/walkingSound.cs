110e804214b949b00f98c406b793410267b1ae8b
sondrex <sondrewr@stud.ntnu.no>
2019-09-08 18:42:32 +0200

    Fixed walking sound, I bloody forgot about velocity

--- Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/walkingSound.cs
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/walkingSound.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class walkingSound : MonoBehaviour
 {
+    public Transform mainCamera;
     public AudioSource audioWalking;
-    public float minDelay = 0.5f;   // Minimum delay
-
-    float currentDelay;
-    Vector3 previousPosition = new Vector3(0, 0, 0);
+    
+    Vector3 stillmovement = new Vector3(0, 0, 0);
     
     // plays sound if the character's movement is not approximately 0
     void Update()
     {
-        if ((previousPosition.x) != (transform.position.x)
-            || (previousPosition.z) != (transform.position.z)) // if the character have moved
-        {
-            currentDelay += Time.deltaTime;
-
-            // Checks if enough time have passed
-            if (currentDelay > minDelay)
-            {
-                previousPosition = transform.position;  // updates position
-            }
-
-            audioWalking.mute = false;
-        }
-        else
-        {
-            if (currentDelay > minDelay)
-            {
-                audioWalking.mute = true;
-            }
-            currentDelay = 0;
-        }
-        
-        // Debug.Log(previousPosition); // Remember to remove this!
+        // Mutes the osund if the player is still
+        audioWalking.mute = mainCamera.GetComponent<Rigidbody>().velocity == stillmovement;
     }
 }
