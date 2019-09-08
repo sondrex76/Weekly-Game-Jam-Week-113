d7827db2c21e197d1c3b2d0e70deacf8217d254d
sondrex <sondrewr@stud.ntnu.no>
2019-09-08 16:46:40 +0200

    Fixed victory screen and modified hitbox of melee opponents

--- Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/checkVictory.cs
+++ Weekly Game Jam Week 113-5 Minute Hero/Assets/Scripts/checkVictory.cs
@@ -7,7 +7,13 @@ public class checkVictory : MonoBehaviour
 {
     void Update()
     {
-        if (transform.childCount == 0) {    // Checks if all children(enemies) are dead
+        if (transform.childCount == 0)  // Checks if all children(enemies) are dead
+        {   
+            // Resets cursor
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+
+            // Changes scene
             SceneManager.LoadScene("YouWon");
         }
     }
